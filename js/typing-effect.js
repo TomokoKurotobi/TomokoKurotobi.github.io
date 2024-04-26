d0e73cc0b29d5925629e59df84e69ce830b27051
SleepyCat-00 <mkuroda987@gmail.com>
2024-04-26 06:33:14 -0600

    Cleaned up typing effect function

--- js/typing-effect.js
+++ js/typing-effect.js
@@ -26,41 +26,40 @@ function typingEffect(id, originalText, addingText, typingSpeed, timeout) {
     }
     return new Promise(resolve => setTimeout(resolve, addingText.length * typingSpeed + timeout));
 }
-async function runTypingEffects() {
+async function typeBackground() {
     const lastLogin = getLastLoginStr();
     const console_text_prefix = 'Tomoko@World ~ % ';
     let console_text = '';
     const target_id = 'terminal-contents';
-    await typingEffect(target_id, console_text, lastLogin, 0, 0);
-    console_text += lastLogin + '<br>';
-    await typingEffect(target_id, console_text, console_text_prefix, 0, 0);
-    console_text += console_text_prefix;
-    await typingEffect(target_id, console_text, 'ls', 50, 500);
-    console_text += 'ls<br>';
-    await typingEffect(target_id, console_text, 'information.txt', 0, 0);
-    console_text += 'information.txt<br>';
-    await typingEffect(target_id, console_text, console_text_prefix, 0, 0);
-    console_text += console_text_prefix;
-    await typingEffect(target_id, console_text, 'cat information.txt', 50, 500);
-    console_text += 'cat information.txt<br>';
-    await typingEffect(target_id, console_text, 'Tomoko Kurotobi', 0, 0);
-    console_text += 'Tomoko Kurotobi<br>';
-    await typingEffect(target_id, console_text, 'Software Developer', 0, 0);
-    console_text += 'Software Developer<br>';
-    await typingEffect(target_id, console_text, console_text_prefix, 0, 0);
-    console_text += console_text_prefix;
-    await typingEffect(target_id, console_text, 'echo "#include &lt;iostream&gt;\nint main(){std::cout << "Hello, World!" << std::endl; return 0;}" > hello.cpp', 50, 500);
-    console_text += 'echo "#include &lt;iostream&gt;\nint main(){std::cout << "Hello, World!" << std::endl; return 0;}" > hello.cpp<br>';
-    await typingEffect(target_id, console_text, console_text_prefix, 0, 500);
-    console_text += console_text_prefix;
-    await typingEffect(target_id, console_text, 'g++ hello.cpp -o hello', 50, 500);
-    console_text += 'g++ hello.cpp -o hello<br>';
-    await typingEffect(target_id, console_text, console_text_prefix, 0, 0);
-    console_text += console_text_prefix;
-    await typingEffect(target_id, console_text, './hello', 50, 500);
-    console_text += './hello<br>';
-    await typingEffect(target_id, console_text, 'Hello, World!', 0, 0);
-    console_text += 'Hello, World!<br>';
-    await typingEffect(target_id, console_text, console_text_prefix, 0, 0);
+    const new_line = '<br>';
+
+    const typing_list = [
+        {contents: lastLogin, typingSpeed: 0, timeout: 0, break: true},
+        {contents: console_text_prefix, typingSpeed: 0, timeout: 0, break: false},
+        {contents: 'ls', typingSpeed: 50, timeout: 500, break: true},
+        {contents: 'information.txt', typingSpeed: 0, timeout: 0, break: true},
+        {contents: console_text_prefix, typingSpeed: 0, timeout: 0, break: false},
+        {contents: 'cat information.txt', typingSpeed: 50, timeout: 500, break: true},
+        {contents: 'Tomoko Kurotobi', typingSpeed: 0, timeout: 0, break: true},
+        {contents: 'Software Developer', typingSpeed: 0, timeout: 0, break: true},
+        {contents: console_text_prefix, typingSpeed: 0, timeout: 0, break: false},
+        {contents: 'echo "#include &lt;iostream&gt;\nint main(){std::cout << "Hello, World!" << std::endl; return 0;}" > hello.cpp', typingSpeed: 50, timeout: 500, break: true},
+        {contents: console_text_prefix, typingSpeed: 0, timeout: 0, break: false},
+        {contents: 'g++ hello.cpp -o hello', typingSpeed: 50, timeout: 500, break: true},
+        {contents: console_text_prefix, typingSpeed: 0, timeout: 0, break: false},
+        {contents: './hello', typingSpeed: 50, timeout: 500, break: true},
+        {contents: 'Hello, World!', typingSpeed: 0, timeout: 0, break: true},
+        {contents: console_text_prefix, typingSpeed: 0, timeout: 0, break: false},
+    ];
+
+    for (let i = 0; i < typing_list.length; i++) {
+        const text = typing_list[i];
+        await typingEffect(target_id, console_text, text.contents, text.typingSpeed, text.timeout);
+        console_text += text.contents ;
+        if (text.break) {
+            console_text += new_line;
+        }
+    }
+    
 }
 
