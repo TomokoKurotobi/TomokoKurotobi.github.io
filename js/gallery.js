2c13eaad57667d98bdec341f20ec0888526dcdd4
SleepyCat-00 <mkuroda987@gmail.com>
2024-04-26 15:43:22 -0600

    Define colors

--- js/gallery.js
+++ js/gallery.js
@@ -1,73 +1,4 @@
 
-function createGalleryItem(id, src, alt, caption) {
-  let galleryItem = document.createElement('li');
-  galleryItem.className = 'gallery-item';
-  galleryItem.id = id;
-
-  let image = document.createElement('img');
-  image.src = src;
-  image.alt = alt;
-
-  let figCaption = document.createElement('figcaption');
-  figCaption.textContent = caption;
-
-  galleryItem.appendChild(image);
-  galleryItem.appendChild(figCaption);
-
-  return galleryItem;
-}
-
-function createPopup(id, src, alt, heading, text) {
-  let popup = document.createElement('div');
-  popup.className = 'popup';
-  popup.id = id + '-popup';
-
-  // Close button, previous button, next button
-  let cross = document.createElement('div');
-  cross.className = 'fa-solid fa-xmark btn-cross';
-  cross.id = 'cross';
-
-  let prv = document.createElement('div');
-  prv.className = 'fa-solid fa-chevron-left btn-prv';
-  prv.id = 'prv';
-
-  let nxt = document.createElement('div');
-  nxt.className = 'fa-solid fa-chevron-right btn-nxt';
-  nxt.id = 'nxt';
-
-  // Popup content
-  let popupContent = document.createElement('div');
-  popupContent.className = 'popup-content';
-
-  let h2 = document.createElement('h2');
-
-  let image = document.createElement('img');
-  image.src = src;
-  image.alt = alt;
-  h2.appendChild(image);
-
-  let textNode = document.createTextNode(heading);
-  h2.appendChild(textNode);
-
-  let p = document.createElement('p');
-  p.textContent = text;
-
-  let button = document.createElement('button');
-  button.className = 'btn btn-lg btn-primary pull-right';
-  button.id = 'close';
-  button.textContent = 'Close';
-
-  popupContent.appendChild(h2);
-  popupContent.appendChild(p);
-  popupContent.appendChild(button);
-
-  popup.appendChild(cross);
-  popup.appendChild(prv);
-  popup.appendChild(nxt);
-  popup.appendChild(popupContent);
-
-  return popup;
-}
 
 var items = [
   {
@@ -175,6 +106,14 @@ var items = [
     text: 'This is a popup window!'
   },
   {
+    id: 'visualstudio',
+    src: 'figures/VisualStudio_Logo.png',
+    alt: 'IDE',
+    caption: 'Visual Studio',
+    heading: 'Visual Studio IDE',
+    text: 'This is a popup window!'
+  },
+  {
     id: 'ubuntu',
     src: 'figures/Ubuntu_Logo.png',
     alt: 'Operating System',
@@ -208,6 +147,60 @@ var items = [
   }
 ];
 
+function createGalleryItem(id, src, alt, caption) {
+  let galleryItem = document.createElement('li');
+  galleryItem.className = 'gallery-item';
+  galleryItem.id = id;
+
+  let image = document.createElement('img');
+  image.src = src;
+  image.alt = alt;
+
+  let figCaption = document.createElement('figcaption');
+  figCaption.textContent = caption;
+
+  galleryItem.appendChild(image);
+  galleryItem.appendChild(figCaption);
+
+  return galleryItem;
+}
+
+function createPopup(id, src, alt, heading, text) {
+  let popup = document.createElement('div');
+  popup.className = 'popup';
+  popup.id = id + '-popup';
+
+  // Popup content
+  let popupContent = document.createElement('div');
+  popupContent.className = 'popup-content';
+
+  let h2 = document.createElement('h2');
+
+  let image = document.createElement('img');
+  image.src = src;
+  image.alt = alt;
+  h2.appendChild(image);
+
+  let textNode = document.createTextNode(heading);
+  h2.appendChild(textNode);
+
+  let p = document.createElement('p');
+  p.textContent = text;
+
+  let button = document.createElement('button');
+  button.className = 'btn btn-lg btn-primary pull-right';
+  button.id = 'close';
+  button.textContent = 'Close';
+
+  popupContent.appendChild(h2);
+  popupContent.appendChild(p);
+  popupContent.appendChild(button);
+
+  popup.appendChild(popupContent);
+
+  return popup;
+}
+
 function createGallery() {
   let gallery = document.getElementById('gallery-grid');
   for (let i = 0; i < items.length; i++) {
@@ -215,34 +208,84 @@ function createGallery() {
     let galleryItem =
         createGalleryItem(item.id, item.src, item.alt, item.caption);
     gallery.appendChild(galleryItem);
-    let popup =
-        createPopup(item.id, item.src, item.alt, item.heading, item.text);
-    gallery.appendChild(popup);
+    // let popup =
+    //     createPopup(item.id, item.src, item.alt, item.heading, item.text);
+    // gallery.appendChild(popup);
+    // let buttons = createPopupButtons();
+    // popup.appendChild(buttons);
   }
 }
+
+
+function createPopupButtons() {
+  let buttons = document.createElement('div');
+  buttons.className = 'buttons';
+
+  let cross = document.createElement('div');
+  cross.className = 'fa-solid fa-xmark btn-cross';
+  cross.id = 'cross';
+
+  let prv = document.createElement('div');
+  prv.className = 'fa-solid fa-chevron-left btn-prv';
+  prv.id = 'prv';
+
+  let nxt = document.createElement('div');
+  nxt.className = 'fa-solid fa-chevron-right btn-nxt';
+  nxt.id = 'nxt';
+
+  buttons.appendChild(cross);
+  buttons.appendChild(prv);
+  buttons.appendChild(nxt);
+  return buttons;
+}
+
+function createGalleryPopups() {
+  let popups = document.getElementById('gallery-popups');
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i];
+    let popup =
+        createPopup(item.id, item.src, item.alt, item.heading, item.text);
+    popups.appendChild(popup);
+    let buttons = createPopupButtons();
+    popup.appendChild(buttons);
+  }
+}
+
 createGallery();
+createGalleryPopups();
 
 $(document).ready(function() {
   items.forEach(function(item, index) {
     var popup = $('#' + item.id + '-popup');
-    $('#' + item.id).click(function() {
-      popup.show();
-    });
-    popup.find('#cross').click(function() {
-      popup.hide();
+    $('#' + item.id).click(function(e) {
+      e.stopPropagation();
+      popup.css('opacity', 0);
+      popup.css('display', 'block');
+      popup.animate({opacity: 1}, 500);
     });
     popup.find('#close').click(function() {
+      popup.hide();
+    });
+    popup.find('#cross').click(function() {
       popup.hide();
     });
     popup.find('#prv').click(function() {
       popup.hide();
       var prevIndex = (index - 1 + items.length) % items.length;
-      $('#' + items[prevIndex].id + '-popup').show();
+      var prevPopup = $('#' + items[prevIndex].id + '-popup');
+      // move from left to right
+      prevPopup.css('left', '-100%');
+      prevPopup.show();
+      prevPopup.animate({left: '0'}, 500);
     });
     popup.find('#nxt').click(function() {
       popup.hide();
       var nextIndex = (index + 1) % items.length;
-      $('#' + items[nextIndex].id + '-popup').show();
+      var nextPopup = $('#' + items[nextIndex].id + '-popup');
+      // move from right to left
+      nextPopup.css('left', '100%');
+      nextPopup.show();
+      nextPopup.animate({left: '0'}, 500);
     });
   });
 });
